7d2d89ab06656ed6ac41d0a97e80dff1d82b7795
André Marques <andre.m.d.abreu@gmail.com>
2023-08-30 17:46:53 -0300

    Atualização no layout

--- produtos/visualizar.php
+++ produtos/visualizar.php
@@ -9,14 +9,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos - Visualização</title>
-    <style>
-        * {box-sizing: border-box;}
-        .produtos {
-            display: grid;
-            grid-template-columns: repeat(3, 1fr);
-            gap: 30px;
-        }
-    </style>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
@@ -29,21 +21,22 @@
         <h2>Lendo e carregando todos os produtos.</h2>
         <p><a href="inserir.php" class="btn btn-primary">Inserir novo produto</a></p>
 
-        <section class="produtos">
+        <section class="row justify-content-around">
             <?php foreach($listaDeProdutos as $produto){ ?>
-                <article class="produto card border-primary shadow">
+                <article class="card border-primary col-11 col-sm-5 col-lg-3 m-1 shadow">
                     <div class="card-header">
-                        <h3><?=$produto["produto"]?></h3>
-                        <h4><?=$produto["fabricante"]?></h4>
+                        <h3 class="card-title"><?=$produto["produto"]?></h3>
+                        <h4 class="card-subtitle text-body-secondary"><?=$produto["fabricante"]?></h4>
                     </div>
                     <div class="card-body text-primary">
-                        <p><b>Preço: </b><?=formatarPreco($produto["preco"])?></p>
-                        <p><b>Quantidade: </b><?=$produto["quantidade"]?></p>
-                        <p><b>Preço total: </b><?=formatarPreco($produto["preco_total"])?></p>
+                        <p class="card-text"><b>Preço: </b><?=formatarPreco($produto["preco"])?></p>
+                        <p class="card-text"><b>Quantidade: </b><?=$produto["quantidade"]?></p>
+                        <p class="card-text"><b>Preço total: </b><?=formatarPreco($produto["preco_total"])?></p>
                     </div>
                     <div class="card-footer">
-                        <p class="text-center">
-                            <a href="atualizar.php?id=<?=$produto["id"]?>" class="btn btn-primary"><i class="bi bi-pencil"></i> Editar</a> <a href="excluir.php?id=<?=$produto["id"]?>" class="btn btn-primary"><i class="bi bi-trash"></i> Excluir</a>
+                        <p class="card-link row justify-content-around">
+                            <a href="atualizar.php?id=<?=$produto["id"]?>" class="col-5 btn btn-primary"><i class="bi bi-pencil"></i> Editar</a>
+                            <a href="excluir.php?id=<?=$produto["id"]?>" class="col-5 btn btn-primary"><i class="bi bi-trash"></i> Excluir</a>
                         </p>
                     </div>
                 </article>
